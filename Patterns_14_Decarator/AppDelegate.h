309b3e3258260448bcc9c1bf44874547476005df
Ильяс <i.almakaev@gmail.com>
2017-08-17 21:49:20 +0700

    Initial Commit

---
+++ Patterns_14_Decarator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Patterns_14_Decarator
+//
+//  Created by Ильяс on 17.08.17.
+//  Copyright © 2017 Алмакаев Ильяс. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
